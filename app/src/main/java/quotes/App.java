435d04d6ceafbf2e6ddfa12ed37107e9e7ae8e5f
motasemZaidAlkilani <m.kilany1999@gmail.com>
2022-03-14 11:47:18 +0300

    quoteFromApi

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -4,12 +4,16 @@
 package quotes;
 
 
+import java.io.IOException;
+
+
 public class App {
 
 
-    public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) throws IOException {
+getAndSaveQuoteLocally first_quote=new getAndSaveQuoteLocally("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
+System.out.println(getAndSaveQuoteLocally.quotes);
     }
+
+
 }
